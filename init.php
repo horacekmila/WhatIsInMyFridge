ea5af98dffddba069cbe3adfb0e299c630308d65
David Jungman <dawettoxin@seznam.cz>
2017-08-04 15:59:07 +0200

    Gitignore & init + config

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+ /* Here are files, which are going to be on every page
+        -> loading classes
+        -> configuration of database
+        -> css, js
+*/
+
+require_once("config/config.mysql.php");
